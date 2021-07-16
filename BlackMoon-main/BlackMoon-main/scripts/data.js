135d7a08356f76a66dba0482beaa81a1bd3e95a5
Fugur <74984602+Fugur@users.noreply.github.com>
2021-07-16 19:15:54 +0300

    Delete BlackMoon-main/BlackMoon-main directory

--- BlackMoon-main/BlackMoon-main/scripts/data.js
+++
@@ -1,6 +0,0 @@
-setInterval(function (){
-    var dateCrah = new Date("September 11, 2001 12:00:00").getTime();
-    var now = new Date().getTime();
-    var time = now-dateCrah;
-    document.querySelector("p").innerHTML = time;
-}, 1000);
